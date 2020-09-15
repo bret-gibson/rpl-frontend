627c7a31b0e1fbabd92448f9ab162b2366ab9066
bret-gibson <bret.gibson@gmail.com>
2020-09-14 21:00:27 -0400

    username only login page

--- src/App.js
+++ src/App.js
@@ -1,26 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "semantic-ui-css/semantic.min.css";
+import logo from "./logo.svg";
+import "./App.css";
+import { Route, Switch } from "react-router-dom";
+import Login from "./components/Login";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      userData: null,
+    };
+  }
+
+  submitUser = (userObj) => {
+    fetch("http://localhost:3000/users")
+      .then((response) => response.json())
+      .then((users) => {
+        const user = users.find((user) => user.username === userObj.username);
+        fetch(`http://localhost:3000/users/${user.id}`)
+          .then((response) => response.json())
+          .then((userData) => {
+            this.setState({ userData });
+          });
+      });
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <Route
+          exact
+          path="/"
+          render={() => (
+            <Login
+              submitUser={this.submitUser}
+              userData={this.state.userData}
+            />
+          )}
+        />
+      </div>
+    );
+  }
 }
 
 export default App;
